6963830944632eb3a0ddc91f5798f595b8df5841
Nazar <tzgfjxgv@gmail.com>
2023-06-27 20:05:31 +0300

    Створив джс файл з массивом продуктів

---
+++ index.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id:1,
+        price:200,
+        src:'https://m.media-amazon.com/images/I/71jG+e7roXL._AC_UL320_.jpg',
+        name:'Apple 2020 MacBook Air Laptop M1 Chip, 13" Retina Display, 8GB RAM',
+        category:'laptop'
+    },
+    {
+        id:2,
+        price:300,
+        src:'https://m.media-amazon.com/images/I/819hAP4xoGS._AC_UL320_.jpg',
+        name:'Lenovo 2022 Newest Ideapad 3 Laptop, 15.6" HD Touchscreen,',
+        category:'phone'
+    },
+    {
+        id:3,
+        price:2500,
+        src:'https://m.media-amazon.com/images/I/718B6zl+b6L._AC_UL320_.jpg',
+        name:'SAMSUNG Galaxy Tab S6 Lite 10.4" 64GB Android Tablet',
+        category:'phone'
+    },
+    {
+        id:4,
+        price:1500,
+        src:'https://m.media-amazon.com/images/I/610Pe9IL51L._AC_UY218_.jpg',
+        name:'Withings ScanWatch - Hybrid Smartwatch & Activity Tracker',
+        category:'watch'
+    },
+    {
+        id:5,
+        price:400,
+        src:'https://m.media-amazon.com/images/I/51uTzQauJOL._AC_SX425_.jpg',
+        name:'MorePro Smart Watch, Fitness Tracker with Heart Rate Blood Pressure Monitor',
+        category:'watch'
+    },
+    {
+        id:6,
+        price:200,
+        src:'https://m.media-amazon.com/images/I/41W70+otIaL._AC_UL1500_.jpg',
+        name:"Oakley Men's Oo9102 Holbrook Square Sunglasses",
+        category:'glass'
+    },
+];
